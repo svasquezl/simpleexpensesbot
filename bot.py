694148235d7e42ec5c88bc9e0f5d85a560211393
svasquez <svasquez@transfieriendo.co>
2021-02-06 12:16:08 -0500

    Se adicional modelos y logica de base de datos

--- bot.py
+++ bot.py
@@ -4,35 +4,61 @@ import config
 from time import sleep
 import re
 import logic
-#########################################################
-# Aquí vendrá la implementación de la lógica del bot
-#########################################################
+import database.db as db
 
-#/start
+#########################################################
+if __name__ == '__main__':
+    db.Base.metadata.create_all(db.engine)
+#########################################################
 @bot.message_handler(commands=['start'])
 def on_command_start(message):
     pass
-
+#########################################################
 @bot.message_handler(commands=['help'])
 def on_command_help(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_message(
+        message.chat.id,
+        logic.get_help_message(),
+        parse_mode="Markdown")
+#########################################################
+@bot.message_handler(commands=['about'])
+def on_command_about(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_message(
+        message.chat.id,
+        logic.get_about_this(config.VERSION),
+        parse_mode="Markdown")
+#########################################################
+@bot.message_handler(regexp=r"^(gane|gané|g) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_earn_money(message):
     pass
-
-
-
+#########################################################
+@bot.message_handler(regexp=r"^(gaste|gasté|gg) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_spend_money(message):
+    pass
+#########################################################
+@bot.message_handler(regexp=r"^(listar ganancias|lg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_earnings(message):
+    pass
+#########################################################
+@bot.message_handler(regexp=r"^(listar gastos|lgg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_spendings(message):
+    pass
+#########################################################
+@bot.message_handler(regexp=r"^(obtener saldo|s)$")
+def on_get_balance(message):
+    pass
+#########################################################
+@bot.message_handler(regexp=r"^(remover|r) (ganancia|g|gasto|gg) ([0-9]+)$")
+def on_remove_record(message):
+    pass
 #########################################################
 @bot.message_handler(func=lambda message: True)
 def on_fallback(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    sleep(1)
-
-    bot.reply_to(
-    message,
-    "\U0001F63F Ups, no entendí lo que me dijiste.")
-
-
-
-
+    pass
 #########################################################
 if __name__ == '__main__':
     bot.polling(timeout=20)
 #########################################################
+
---
+++ logic.py
@@ -0,0 +1,24 @@
+def get_about_this(VERSION):
+  response = (
+  f"Simple Expenses Bot (pyTelegramBot) v{VERSION}"
+  "\n\n"
+  "Desarrollado por Svasquezl <sebas0813@gmail.com>"
+  )
+  return response
+
+def get_help_message ():
+  response = (
+  "Estos son los comandos y órdenes disponibles:\n"
+  "\n"
+  "*/start* - Inicia la interacción con el bot (obligatorio)\n"
+  "*/help* - Muestra este mensaje de ayuda\n"
+  "*/about* - Muestra detalles de esta aplicación\n"
+  "*gane|gané|g {cantidad}* - Registra un saldo positivo\n"
+  "*gaste|gasté|gg {cantidad}* - Registra un saldo negativo\n"
+  "*listar ganancias|lg en {índice_mes} de {año}* - Lista las  ganancias de un mes/año\n"
+  "*listar gastos|lgg en {mes} de {año}* - Lista los gastos de un  mes/año\n"
+  "*obtener saldo|s* - Muestra el saldo actual (disponible)\n"
+  "*remover|r ganancia|g|gasto|gg {índice}* - Remueve una ganancia o un gasto según su índice\n"
+  "*listar cuentas|lc* - Lista las cuentas registradas (sólo admin)\n"
+  )
+  return response
